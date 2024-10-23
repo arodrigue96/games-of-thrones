eecec1d848adbfbe2c901ede1e35e50a7412311a
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-23 12:19:38 +0200

    Create squire type

---
+++ src/characters/squire/types.ts
@@ -0,0 +1,7 @@
+import type { Character } from "../character/types";
+
+export interface Squire extends Character {
+  servedCharacter: "Fighter";
+  pelotismDegree: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+  speak: () => "Soy un loser";
+}
