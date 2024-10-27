aa6d7f1f0fde1ef7ed43802bc19811d69669171a
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-27 23:11:02 +0100

    Display image face down when character is dead

--- src/ui/components/cards/card/components/createCardImage.ts
+++ src/ui/components/cards/card/components/createCardImage.ts
@@ -4,8 +4,12 @@ const createCardImage = (character: Character): HTMLImageElement => {
   const cardImage = document.createElement("img");
   cardImage.classList.add("card__image");
   cardImage.src = character.image;
-  cardImage.alt = `Imagen del personaje ${character.name} ${character.lastName ?? ""}`;
+  cardImage.alt = `Character image of ${character.name} ${character.lastName ?? ""}`;
   cardImage.width = 340;
+
+  if (!character.isAlive) {
+    cardImage.classList.add("card__image--dead");
+  }
 
   return cardImage;
 };
