9a4e96042fa98b125788eab60dde6a9a5aff2db0
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-27 22:49:47 +0100

    Add character state when is dead

--- src/ui/components/cards/card/components/createCardState.ts
+++ src/ui/components/cards/card/components/createCardState.ts
@@ -12,9 +12,19 @@ const createCardState = (character: Character): HTMLDivElement => {
   if (character.isAlive) {
     const thumbUp = document.createElement("img");
     thumbUp.classList.add("card__state--alive");
-    thumbUp.src = "icons/thumb-up-line.svg";
+    thumbUp.src = "icons/thumb-up-fill.svg";
     cardInfo.appendChild(thumbUp);
     thumbUp.alt = "Character is alive";
+    thumbUp.width = 20;
+    thumbUp.height = 20;
+  }
+
+  if (!character.isAlive) {
+    const thumbUp = document.createElement("img");
+    thumbUp.classList.add("card__state--dead");
+    thumbUp.src = "icons/thumb-down-fill.svg";
+    cardInfo.appendChild(thumbUp);
+    thumbUp.alt = "Character is dead";
     thumbUp.width = 20;
     thumbUp.height = 20;
   }
