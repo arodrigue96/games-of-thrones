5f3e9e3e24a1080707cca3de4f0dd2121679b8ee
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-30 23:52:36 +0100

    Refactor createCardState function

--- src/characters/components/card/index.ts
+++ src/characters/components/card/index.ts
@@ -60,33 +60,19 @@ export const createCardState = (character: Character): HTMLDivElement => {
   const cardInfo = document.createElement("div");
   cardInfo.classList.add("card__info");
 
-  const characterState = document.createElement("span");
-  characterState.classList.add("card__state");
-  characterState.textContent = "State:";
-  cardInfo.appendChild(characterState);
+  cardInfo.innerHTML = `
+  <span class= "card__state">
+    State:
+    <img
+      src="icons/${character.isAlive ? "thumb-up-fill.svg" : "thumb-down-fill.svg"}"
+      alt="Character is ${character.isAlive ? "Character is alive" : "Character is dead"}"
+      width="20"
+      height="20"
+    >
+  </span>
+  `;
 
-  if (character.isAlive) {
-    const thumbUp = document.createElement("img");
-    thumbUp.classList.add("card__state--alive");
-    thumbUp.src = "icons/thumb-up-fill.svg";
-    thumbUp.alt = "Character is alive";
-    thumbUp.width = 20;
-    thumbUp.height = 20;
-    cardInfo.appendChild(thumbUp);
-  }
-
-  if (!character.isAlive) {
-    const thumbDown = document.createElement("img");
-    thumbDown.classList.add("card__state--dead");
-    thumbDown.src = "icons/thumb-down-fill.svg";
-    thumbDown.alt = "Character is dead";
-    thumbDown.width = 20;
-    thumbDown.height = 20;
-    cardInfo.appendChild(thumbDown);
-  }
-
-  const emojiContainer = getCharacterEmoji(character);
-  cardInfo.appendChild(emojiContainer);
+  cardInfo.appendChild(getCharacterEmoji(character));
 
   return cardInfo;
 };
