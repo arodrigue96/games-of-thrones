4b1d90ca50491bcdbe9770f5ab737fd3a2778c26
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-02 12:10:43 +0100

    Create Advisor character class

---
+++ src/characters/advisor/Advisor/types.ts
@@ -0,0 +1,5 @@
+import { type Character } from "../../character/Character/Character";
+
+export interface AdvisorStructure {
+  advisedCharacter: Character;
+}
--- src/characters/advisor/types.ts
+++
@@ -1,5 +0,0 @@
-import type { Character } from "../character/types";
-
-export interface Advisor extends Character {
-  advisedCharacter: Character;
-}
