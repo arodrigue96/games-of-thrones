13002f959b170a9e4875c424c200b992f423e65d
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-02 12:07:44 +0100

    Create Fighter character class

--- src/characters/fighter/Fighter/types.ts
+++ src/characters/fighter/Fighter/types.ts
@@ -1,8 +1,6 @@
-import type { Character } from "../character/types";
-
 export type SkillLevel = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
-export interface Fighter extends Character {
+export interface FighterStructure {
   weapon: string;
   skillLevel: SkillLevel;
 }
